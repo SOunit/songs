787d268e708c156746a6abe87839b88a1460647e
SOUnit <workbookwork7@gmail.com>
2021-03-21 12:07:45 +0900

    added reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: 'song1', duration: '1:05' },
+    { title: 'song2', duration: '2:05' },
+    { title: 'song3', duration: '3:05' },
+    { title: 'song4', duration: '4:05' },
+  ];
+};
